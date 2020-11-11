a4be4303954a63c9cb52bc9113920456ee075bc2
johannesEZ <74159826+johannesEZ@users.noreply.github.com>
2020-11-11 20:07:35 +0100

    First Commit

---
+++ Assets/Scripts/TargetFPS.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFPS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Application.targetFrameRate = 99999;
+        QualitySettings.vSyncCount = 0;
+    }
+}
